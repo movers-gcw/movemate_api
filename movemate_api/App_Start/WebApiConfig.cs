6b2017300f8f95ac997bd35edb3002d4d1f2d4a8
Andrea Bagordo <andrewbaghi@gmail.com>
2017-02-05 15:46:58 +0100

    aggiunto routing per richiamo metodi

--- movemate_api/App_Start/WebApiConfig.cs
+++ movemate_api/App_Start/WebApiConfig.cs
@@ -22,7 +22,7 @@ namespace movemate_api
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
