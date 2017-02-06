375a4a7a3e651a70be017c147ac47a52b28f8c12
WSAndrea <andrewbaghi@gmail.com>
2017-02-06 16:40:17 +0100

    poststudent con check funzionante

--- movemate_api/App_Start/WebApiConfig.cs
+++ movemate_api/App_Start/WebApiConfig.cs
@@ -25,6 +25,7 @@ namespace movemate_api
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
         }
     }
--- movemate_api/Migrations/201702021356119_Initial.cs
+++ movemate_api/Migrations/201702021356119_Initial.cs
@@ -7,23 +7,45 @@ namespace movemate_api.Migrations
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
             
             CreateTable(
                 "dbo.Students",
                 c => new
                     {
-                        StudentId = c.Int(nullable: false, identity: true),
+                        StudentId = c.Int(nullable: false),
+                        FacebookId = c.String(nullable: false, maxLength: 128),
                         Name = c.String(),
                         Surname = c.String(),
                         Email = c.String(),
                         Verified = c.Boolean(nullable: false),
                         VerificationCode = c.String(),
-                        FacebookId = c.String(),
                         GoogleId = c.String(),
                         Department_DepartmentId = c.Int(),
                         University_universityId = c.Int(),
                     })
-                .PrimaryKey(t => t.StudentId)
+                .PrimaryKey(t => new { t.StudentId, t.FacebookId })
                 .ForeignKey("dbo.Departments", t => t.Department_DepartmentId)
                 .ForeignKey("dbo.Universities", t => t.University_universityId)
                 .Index(t => t.Department_DepartmentId)
@@ -63,27 +85,82 @@ namespace movemate_api.Migrations
                     })
                 .PrimaryKey(t => t.universityId);
             
-           
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
         }
         
         public override void Down()
         {
-           
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.Students", "University_universityId", "dbo.Universities");
             DropForeignKey("dbo.Departments", "University_universityId", "dbo.Universities");
             DropForeignKey("dbo.Students", "Department_DepartmentId", "dbo.Departments");
             DropForeignKey("dbo.PointOfInterests", "Department_DepartmentId", "dbo.Departments");
-            
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
             DropIndex("dbo.PointOfInterests", new[] { "Department_DepartmentId" });
             DropIndex("dbo.Departments", new[] { "University_universityId" });
             DropIndex("dbo.Students", new[] { "University_universityId" });
             DropIndex("dbo.Students", new[] { "Department_DepartmentId" });
-            
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
             DropTable("dbo.Universities");
             DropTable("dbo.PointOfInterests");
             DropTable("dbo.Departments");
             DropTable("dbo.Students");
-            
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetRoles");
         }
     }
 }
