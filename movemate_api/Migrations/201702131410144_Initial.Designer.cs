9c8b46e5e30f2634df52d4901f45a1a96f8f828c
WSAndrea <andrewbaghi@gmail.com>
2017-02-13 16:24:42 +0100

    EntityFramework.Seeder implementato, ma seeding delle sedi non funzionante

--- movemate_api/Migrations/201702131410144_Initial.Designer.cs
+++ movemate_api/Migrations/201702131410144_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace movemate_api.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Initial2 : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial2));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201702061221108_Initial2"; }
+            get { return "201702131410144_Initial"; }
         }
         
         string IMigrationMetadata.Source
--- movemate_api/Migrations/201702131410144_Initial.cs
+++ movemate_api/Migrations/201702131410144_Initial.cs
@@ -8,6 +8,42 @@ namespace movemate_api.Migrations
         public override void Up()
         {
             CreateTable(
+                "dbo.Departments",
+                c => new
+                    {
+                        DepartmentId = c.Int(nullable: false, identity: true),
+                        DepartmentName = c.String(),
+                        Address = c.String(),
+                        PlaceId = c.String(),
+                        University_UniversityId = c.Int(),
+                    })
+                .PrimaryKey(t => t.DepartmentId)
+                .ForeignKey("dbo.Universities", t => t.University_UniversityId)
+                .Index(t => t.University_UniversityId);
+            
+            CreateTable(
+                "dbo.Universities",
+                c => new
+                    {
+                        UniversityId = c.Int(nullable: false, identity: true),
+                        UniversityName = c.String(),
+                    })
+                .PrimaryKey(t => t.UniversityId);
+            
+            CreateTable(
+                "dbo.PointOfInterests",
+                c => new
+                    {
+                        PointOfInterestId = c.Int(nullable: false, identity: true),
+                        PlaceId = c.String(),
+                        Latitude = c.Double(nullable: false),
+                        Longitude = c.Double(nullable: false),
+                        Address = c.String(),
+                        DateTime = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.PointOfInterestId);
+            
+            CreateTable(
                 "dbo.AspNetRoles",
                 c => new
                     {
@@ -34,56 +70,22 @@ namespace movemate_api.Migrations
                 "dbo.Students",
                 c => new
                     {
-                        StudentId = c.Int(nullable: false),
-                        FacebookId = c.String(nullable: false, maxLength: 128),
+                        StudentId = c.Int(nullable: false, identity: true),
                         Name = c.String(),
                         Surname = c.String(),
                         Email = c.String(),
                         Verified = c.Boolean(nullable: false),
                         VerificationCode = c.String(),
+                        FacebookId = c.String(),
                         GoogleId = c.String(),
                         Department_DepartmentId = c.Int(),
-                        University_universityId = c.Int(),
+                        University_UniversityId = c.Int(),
                     })
-                .PrimaryKey(t => new { t.StudentId, t.FacebookId })
+                .PrimaryKey(t => t.StudentId)
                 .ForeignKey("dbo.Departments", t => t.Department_DepartmentId)
-                .ForeignKey("dbo.Universities", t => t.University_universityId)
+                .ForeignKey("dbo.Universities", t => t.University_UniversityId)
                 .Index(t => t.Department_DepartmentId)
-                .Index(t => t.University_universityId);
-            
-            CreateTable(
-                "dbo.Departments",
-                c => new
-                    {
-                        DepartmentId = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                        University_universityId = c.Int(),
-                    })
-                .PrimaryKey(t => t.DepartmentId)
-                .ForeignKey("dbo.Universities", t => t.University_universityId)
-                .Index(t => t.University_universityId);
-            
-            CreateTable(
-                "dbo.PointOfInterests",
-                c => new
-                    {
-                        PointOfInterestId = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                        PlaceId = c.String(),
-                        Department_DepartmentId = c.Int(),
-                    })
-                .PrimaryKey(t => t.PointOfInterestId)
-                .ForeignKey("dbo.Departments", t => t.Department_DepartmentId)
-                .Index(t => t.Department_DepartmentId);
-            
-            CreateTable(
-                "dbo.Universities",
-                c => new
-                    {
-                        universityId = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                    })
-                .PrimaryKey(t => t.universityId);
+                .Index(t => t.University_UniversityId);
             
             CreateTable(
                 "dbo.AspNetUsers",
@@ -137,30 +139,28 @@ namespace movemate_api.Migrations
             DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
-            DropForeignKey("dbo.Students", "University_universityId", "dbo.Universities");
-            DropForeignKey("dbo.Departments", "University_universityId", "dbo.Universities");
+            DropForeignKey("dbo.Students", "University_UniversityId", "dbo.Universities");
             DropForeignKey("dbo.Students", "Department_DepartmentId", "dbo.Departments");
-            DropForeignKey("dbo.PointOfInterests", "Department_DepartmentId", "dbo.Departments");
             DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Departments", "University_UniversityId", "dbo.Universities");
             DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
             DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
             DropIndex("dbo.AspNetUsers", "UserNameIndex");
-            DropIndex("dbo.PointOfInterests", new[] { "Department_DepartmentId" });
-            DropIndex("dbo.Departments", new[] { "University_universityId" });
-            DropIndex("dbo.Students", new[] { "University_universityId" });
+            DropIndex("dbo.Students", new[] { "University_UniversityId" });
             DropIndex("dbo.Students", new[] { "Department_DepartmentId" });
             DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
             DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
             DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Departments", new[] { "University_UniversityId" });
             DropTable("dbo.AspNetUserLogins");
             DropTable("dbo.AspNetUserClaims");
             DropTable("dbo.AspNetUsers");
-            DropTable("dbo.Universities");
-            DropTable("dbo.PointOfInterests");
-            DropTable("dbo.Departments");
             DropTable("dbo.Students");
             DropTable("dbo.AspNetUserRoles");
             DropTable("dbo.AspNetRoles");
+            DropTable("dbo.PointOfInterests");
+            DropTable("dbo.Universities");
+            DropTable("dbo.Departments");
         }
     }
 }
