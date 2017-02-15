3b6eacdcefdb7823e9dd351193866b7e0dccb042
WSAndrea <andrewbaghi@gmail.com>
2017-02-15 14:29:07 +0100

    modifiche a pathcontroller

--- movemate_api/Migrations/201702151118239_Initial.cs
+++ movemate_api/Migrations/201702151118239_Initial.cs
@@ -31,6 +31,36 @@ namespace movemate_api.Migrations
                 .PrimaryKey(t => t.UniversityId);
             
             CreateTable(
+                "dbo.Paths",
+                c => new
+                    {
+                        PathId = c.Int(nullable: false, identity: true),
+                        PathName = c.String(),
+                        Vehicle = c.Int(nullable: false),
+                        Train = c.Boolean(nullable: false),
+                        Bus = c.Boolean(nullable: false),
+                        Metro = c.Boolean(nullable: false),
+                        Tram = c.Boolean(nullable: false),
+                        Price = c.String(),
+                        Description = c.String(),
+                        AvailableSeats = c.Int(nullable: false),
+                        AvailableHeadgear = c.Boolean(nullable: false),
+                        Destination_PointOfInterestId = c.Int(),
+                        Student_StudentId = c.Int(),
+                        Maker_StudentId = c.Int(),
+                        Start_PointOfInterestId = c.Int(),
+                    })
+                .PrimaryKey(t => t.PathId)
+                .ForeignKey("dbo.PointOfInterests", t => t.Destination_PointOfInterestId)
+                .ForeignKey("dbo.Students", t => t.Student_StudentId)
+                .ForeignKey("dbo.Students", t => t.Maker_StudentId)
+                .ForeignKey("dbo.PointOfInterests", t => t.Start_PointOfInterestId)
+                .Index(t => t.Destination_PointOfInterestId)
+                .Index(t => t.Student_StudentId)
+                .Index(t => t.Maker_StudentId)
+                .Index(t => t.Start_PointOfInterestId);
+            
+            CreateTable(
                 "dbo.PointOfInterests",
                 c => new
                     {
@@ -39,9 +69,36 @@ namespace movemate_api.Migrations
                         Latitude = c.Double(nullable: false),
                         Longitude = c.Double(nullable: false),
                         Address = c.String(),
-                        DateTime = c.DateTime(nullable: false),
+                        DateTime = c.String(),
+                        Path_PathId = c.Int(),
                     })
-                .PrimaryKey(t => t.PointOfInterestId);
+                .PrimaryKey(t => t.PointOfInterestId)
+                .ForeignKey("dbo.Paths", t => t.Path_PathId)
+                .Index(t => t.Path_PathId);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Surname = c.String(),
+                        Email = c.String(),
+                        Verified = c.Boolean(nullable: false),
+                        VerificationCode = c.String(),
+                        FacebookId = c.String(),
+                        GoogleId = c.String(),
+                        Department_DepartmentId = c.Int(),
+                        University_UniversityId = c.Int(),
+                        Path_PathId = c.Int(),
+                    })
+                .PrimaryKey(t => t.StudentId)
+                .ForeignKey("dbo.Departments", t => t.Department_DepartmentId)
+                .ForeignKey("dbo.Universities", t => t.University_UniversityId)
+                .ForeignKey("dbo.Paths", t => t.Path_PathId)
+                .Index(t => t.Department_DepartmentId)
+                .Index(t => t.University_UniversityId)
+                .Index(t => t.Path_PathId);
             
             CreateTable(
                 "dbo.AspNetRoles",
@@ -65,27 +122,6 @@ namespace movemate_api.Migrations
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId)
                 .Index(t => t.RoleId);
-            
-            CreateTable(
-                "dbo.Students",
-                c => new
-                    {
-                        StudentId = c.Int(nullable: false, identity: true),
-                        Name = c.String(),
-                        Surname = c.String(),
-                        Email = c.String(),
-                        Verified = c.Boolean(nullable: false),
-                        VerificationCode = c.String(),
-                        FacebookId = c.String(),
-                        GoogleId = c.String(),
-                        Department_DepartmentId = c.Int(),
-                        University_UniversityId = c.Int(),
-                    })
-                .PrimaryKey(t => t.StudentId)
-                .ForeignKey("dbo.Departments", t => t.Department_DepartmentId)
-                .ForeignKey("dbo.Universities", t => t.University_UniversityId)
-                .Index(t => t.Department_DepartmentId)
-                .Index(t => t.University_UniversityId);
             
             CreateTable(
                 "dbo.AspNetUsers",
@@ -139,26 +175,39 @@ namespace movemate_api.Migrations
             DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
-            DropForeignKey("dbo.Students", "University_UniversityId", "dbo.Universities");
-            DropForeignKey("dbo.Students", "Department_DepartmentId", "dbo.Departments");
             DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Paths", "Start_PointOfInterestId", "dbo.PointOfInterests");
+            DropForeignKey("dbo.Students", "Path_PathId", "dbo.Paths");
+            DropForeignKey("dbo.Paths", "Maker_StudentId", "dbo.Students");
+            DropForeignKey("dbo.Students", "University_UniversityId", "dbo.Universities");
+            DropForeignKey("dbo.Paths", "Student_StudentId", "dbo.Students");
+            DropForeignKey("dbo.Students", "Department_DepartmentId", "dbo.Departments");
+            DropForeignKey("dbo.Paths", "Destination_PointOfInterestId", "dbo.PointOfInterests");
+            DropForeignKey("dbo.PointOfInterests", "Path_PathId", "dbo.Paths");
             DropForeignKey("dbo.Departments", "University_UniversityId", "dbo.Universities");
             DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
             DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
             DropIndex("dbo.AspNetUsers", "UserNameIndex");
-            DropIndex("dbo.Students", new[] { "University_UniversityId" });
-            DropIndex("dbo.Students", new[] { "Department_DepartmentId" });
             DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
             DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
             DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Students", new[] { "Path_PathId" });
+            DropIndex("dbo.Students", new[] { "University_UniversityId" });
+            DropIndex("dbo.Students", new[] { "Department_DepartmentId" });
+            DropIndex("dbo.PointOfInterests", new[] { "Path_PathId" });
+            DropIndex("dbo.Paths", new[] { "Start_PointOfInterestId" });
+            DropIndex("dbo.Paths", new[] { "Maker_StudentId" });
+            DropIndex("dbo.Paths", new[] { "Student_StudentId" });
+            DropIndex("dbo.Paths", new[] { "Destination_PointOfInterestId" });
             DropIndex("dbo.Departments", new[] { "University_UniversityId" });
             DropTable("dbo.AspNetUserLogins");
             DropTable("dbo.AspNetUserClaims");
             DropTable("dbo.AspNetUsers");
-            DropTable("dbo.Students");
             DropTable("dbo.AspNetUserRoles");
             DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Students");
             DropTable("dbo.PointOfInterests");
+            DropTable("dbo.Paths");
             DropTable("dbo.Universities");
             DropTable("dbo.Departments");
         }
---
+++ movemate_api/Models/PathCarBlob.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace movemate_api.Controllers
+{
+    public class PathCarBlob
+    {
+        public int StudentId { get; set; }
+        public Boolean ToFrom { get; set; }
+        public String PathName { get; set; }
+        public String Date { get; set; }
+        public int DepId { get; set; }
+        public String Address { get; set; }
+        public int Vehicle { get; set; }
+        public Boolean Train { get; set; }
+        public Boolean Bus { get; set; }
+        public Boolean Metro { get; set; }
+        public Boolean Tram { get; set; }
+        public int Seats { get; set; }
+        public String Price { get; set; }
+        public Boolean Head { get; set; }
+        public String Description { get; set; }
+        public Pathblob(int studentId, Boolean toFrom, String pathname, String date, int depId, String address, int vehicle, Boolean train, Boolean bus, Boolean metro, Boolean tram, int seats, String price, Boolean head, String description)
+        {
+            this.StudentId = studentId;
+            this.ToFrom = toFrom;
+            this.PathName = pathname;
+            this.Date = date;
+            this.DepId = depId;
+            this.Address = address;
+            this.Vehicle = vehicle;
+            this.Train = train;
+            this.Bus = bus;
+            this.Metro = metro;
+            this.Tram = tram;
+            this.Seats = seats;
+            this.Price = price;
+            this.Head = head;
+            this.Description = description;
+        }
+        
+    }
+}
